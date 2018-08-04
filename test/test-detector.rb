23d7de8127d967f353919b5c6d21943202ea3dff
yosuke shiro <ys2014hs@gmail.com>
2018-08-04 11:55:17 +0900

    Add detector test case

--- test/test-detector.rb
+++ test/test-detector.rb
@@ -1,7 +1,16 @@
 class TestDetector < Test::Unit::TestCase
+  include Helper::Fixture
+
   def test_new
     assert_nothing_raised do
       DLIB::Detector.new
     end
+  end
+
+  def test_detect
+    image = DLIB::Image.new(fixture_path("sample.jpg").to_s)
+    detector = DLIB::Detector.new
+    rectangles = detector.detect(image)
+    assert_equal(rectangles.size, 1)
   end
 end
