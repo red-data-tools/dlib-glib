dc71fca29691e0ddf8c494da2558cefb40e396a0
yosuke shiro <ys2014hs@gmail.com>
2018-08-27 20:37:37 +0900

    Add test case

--- test/test-rectangle.rb
+++ test/test-rectangle.rb
@@ -4,4 +4,15 @@ class TestRectangle < Test::Unit::TestCase
       DLIB::Rectangle.new
     end
   end
+
+  def test_coordinate
+    rectangle = DLIB::Rectangle.new(1, 2, 3, 4)
+    assert_equal([1, 2, 3, 4],
+                 [
+                   rectangle.left,
+                   rectangle.top,
+                   rectangle.right,
+                   rectangle.bottom
+                 ])
+  end
 end
