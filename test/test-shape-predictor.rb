94ef42984a2df2e53e384f23e27aae6409f7a5df
yosuke shiro <ys2014hs@gmail.com>
2018-09-02 16:28:09 +0900

    Use fixture_path

--- test/test-shape-predictor.rb
+++ test/test-shape-predictor.rb
@@ -3,7 +3,8 @@ class TestShapePredictor < Test::Unit::TestCase
 
   def test_new
     assert_nothing_raised do
-      DLIB::ShapePredictor.new
+      predictor_path = 'shape_predictor_68_face_landmarks.dat'
+      DLIB::ShapePredictor.new(fixture_path(predictor_path).to_s)
     end
   end
 end
