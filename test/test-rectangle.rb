c17c3b10a13da37874eace8c9a8d8b6fd15b8577
Yosuke Shiro <ys2014hs@gmail.com>
2018-09-30 17:52:07 +0900

    Update rectangle test case

--- test/test-rectangle.rb
+++ test/test-rectangle.rb
@@ -1,12 +1,32 @@
 class TestRectangle < Test::Unit::TestCase
-  def test_new
-    assert_nothing_raised do
-      Dlib::Rectangle.new
-    end
+  def test_empty
+    rectangle = Dlib::Rectangle.new
+    assert_equal([0, 0, -1, -1],
+                 [
+                   rectangle.left,
+                   rectangle.top,
+                   rectangle.right,
+                   rectangle.bottom
+                 ])
   end
 
   def test_coordinate
     rectangle = Dlib::Rectangle.new(1, 2, 3, 4)
+    assert_equal([1, 2, 3, 4],
+                 [
+                   rectangle.left,
+                   rectangle.top,
+                   rectangle.right,
+                   rectangle.bottom
+                 ])
+  end
+
+  def test_set_coordinate
+    rectangle = Dlib::Rectangle.new
+    rectangle.left = 1
+    rectangle.top = 2
+    rectangle.right = 3
+    rectangle.bottom = 4
     assert_equal([1, 2, 3, 4],
                  [
                    rectangle.left,
