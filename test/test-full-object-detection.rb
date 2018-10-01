6c8e51013cb69ceb734606693c2feea549f1e92d
Yosuke Shiro <ys2014hs@gmail.com>
2018-10-01 12:06:55 +0900

    Update full-object-detection test case

--- test/test-full-object-detection.rb
+++ test/test-full-object-detection.rb
@@ -1,17 +1,16 @@
 class TestFullObjectDetection < Test::Unit::TestCase
   include Helper::Fixture
 
-  def test_new
-    assert_nothing_raised do
-      rectangle = Dlib::Rectangle.new(1, 2, 3, 4)
-      Dlib::FullObjectDetection.new(rectangle)
-    end
-  end
-
   def test_rectangle
     rectangle = Dlib::Rectangle.new(1, 2, 3, 4)
     full_object_detection = Dlib::FullObjectDetection.new(rectangle)
-    assert_equal(full_object_detection.rectangle.left, 1)
+    assert_equal([1, 2, 3, 4],
+                 [
+                   full_object_detection.rectangle.left,
+                   full_object_detection.rectangle.top,
+                   full_object_detection.rectangle.right,
+                   full_object_detection.rectangle.bottom
+                 ])
   end
 
   def test_n_parts
